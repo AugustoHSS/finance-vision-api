56d8c5bfa4d480759ddfab5c0c030ff180e3bb4a
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2024-12-12 22:33:33 -0300

    feat: add refresh token field to User model and update schema

--- prisma/migrations/20241213013130_add_refresh_token_to_user/migration.sql
+++ prisma/migrations/20241213013130_add_refresh_token_to_user/migration.sql
@@ -1,8 +1,10 @@
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "refreshToken" TEXT,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
