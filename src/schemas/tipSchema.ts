ab5fa59adcd04969af2a76d92c64e7ceacddc124
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2025-01-01 22:50:55 -0300

    feat: create a tip schema with joi

---
+++ src/schemas/tipSchema.ts
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+export const tipSchema = Joi.object({
+    client_id: Joi.number()
+        .integer()
+        .positive(),
+
+    user_id: Joi.number()
+        .integer()
+        .positive()
+        .required(),
+
+    value: Joi.number()
+        .integer()
+        .positive()
+        .required(),
+
+    tip_date: Joi.date()
+        .iso()
+        .required(),
+});
