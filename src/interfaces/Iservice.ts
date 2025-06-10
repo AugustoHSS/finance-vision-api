7a106385df683b9cf3ef799be578cbcdfd910eb2
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2025-06-09 23:20:53 -0300

    create a service interface

---
+++ src/interfaces/Iservice.ts
@@ -0,0 +1,9 @@
+export interface IService {
+    userId: number;
+    serviceDate: Date;
+    value: number;
+    bossId: number;
+    killCount: number;
+    isTicket: boolean;
+    clientId: number;
+}
